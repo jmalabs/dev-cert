fd4187076b722d0d531556d65c6f7bfb0583746a
jmalabs <johnrichard.malabanan.0916@gmail.com>
2023-10-24 23:02:19 +0800

    some changes

---
+++ DevCert.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevCert.WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevCert.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = new List<User> {
+new User { Name ="John Doe", Email ="johnrichard.malabanan.0916@gmail.com"}
+
+            };
+
+            return Ok(users);
+
+        }
+    }
+}
+
---
+++ DevCert.WebAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DevCert.WebAPI.Models
+{
+	public class User
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+	}
+}
+
